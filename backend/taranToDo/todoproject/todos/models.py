6d03f4533df04b1f3324a482045284fb85c62398
Arnav Patel <patelarnavm+github@gmail.com>
2024-11-15 14:39:35 -0500

    added frontend and backend folders

--- backend/taranToDo/todoproject/todos/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Todo(models.Model):
-    title = models.CharField(max_length=200, null=False, blank=False)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
--- backend/toDoAPI/todo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TodoListCreate, TodoDetail
-
-urlpatterns = [
-    path('', TodoListCreate.as_view(), name='todo-list-create'),
-    path('<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
-]
