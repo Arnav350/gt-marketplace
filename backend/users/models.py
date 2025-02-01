32f18c25a2b0c87cf8b41c67ca74290b18865b0a
Kharansh Singh <ksingh380@gatech.edu>
2025-02-01 12:35:56 -0500

    Created Items App, needs integration with Users and review

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    email = models.EmailField(unique=True)
+    avatar = models.URLField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    address_extra = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
