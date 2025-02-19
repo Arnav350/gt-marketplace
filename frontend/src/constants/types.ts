59ef05669963c0b409ba1312b256a9de20784dbd
Arnav Patel <patelarnavm+github@gmail.com>
2025-02-19 12:15:18 -0500

    added types

--- frontend/src/constants/types.ts
+++ frontend/src/constants/types.ts
@@ -2,22 +2,77 @@ import { NavigatorScreenParams } from "@react-navigation/native";
 
 //database
 
+export type TUser = {
+  id: string;
+  username: string;
+  password: string;
+  email: string;
+  name: string;
+  avatar: string | null;
+  address: string | null;
+  address_extra: string | null;
+  created_at: Date;
+};
+
+export type TCategory = "Clothing" | "Electronics" | "Furniture" | "Books" | "Other";
+
 export type TItem = {
   id: string;
   seller_id: string;
   title: string;
   price: number;
-  category: TCategory;
-  condition: TCondition;
+  category: TCategory | null;
+  condition: "Brand New" | "Like New" | "Good" | "Fair" | "Poor" | null;
   description: string | null;
   address: string | null;
   address_extra: string | null;
-  size: TSize | null;
+  size: "Large" | "Medium" | "Small" | null;
   score: number;
   created_at: Date;
   buyer_id: string | null;
   price_sold: number | null;
   sold_at: Date | null;
+  completed_at: Date | null;
+};
+
+export type TSave = {
+  user_id: string;
+  item_id: string;
+  created_at: Date;
+};
+
+export type TDelivery = {
+  id: string;
+  user_id: string;
+  item_id: string;
+  status: "Ordered" | "Retrieved" | "Completed";
+  price: number;
+  address: string;
+  address_extra: string | null;
+  created_at: Date;
+  retrieved_at: Date | null;
+  completed_at: Date | null;
+};
+
+export type TImage = {
+  item_id: string;
+  image: string;
+  created_at: Date;
+};
+
+export type TService = {
+  id: string;
+  user_id: string;
+  package: "Basic" | "Standard" | "Premium";
+  status: "Ordered" | "Checked" | "Confirmed" | "Completed";
+  address: string;
+  address_extra: string | null;
+  created_at: Date;
+  checked_at: Date | null;
+  price: number | null;
+  notes: string | null;
+  timeslot: Date | null;
+  confirmed_at: Date | null;
   completed_at: Date | null;
 };
 
@@ -53,11 +108,3 @@ export type TMessagesStackParamsList = {
   Delivery: undefined;
   Summary: undefined;
 };
-
-//enums
-
-export type TCategory = "Clothing" | "Electronics" | "Furniture" | "Books" | "Other" | null;
-
-export type TCondition = "Brand New" | "Like New" | "Good" | "Fair" | "Poor" | null;
-
-export type TSize = "Large" | "Medium" | "Small" | null;
